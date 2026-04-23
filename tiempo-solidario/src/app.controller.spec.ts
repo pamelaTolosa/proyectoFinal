13242719907184044a9aa8145b81e85c4a3d801e
dianaCiganda <dianaciganda@gmail.com>
2026-04-23 20:57:00 -0300

    estructura de usuario

--- tiempo-solidario/src/app.controller.spec.ts
+++ tiempo-solidario/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './usuario.controller';
+import { AppService } from './usuario.service';
 
 describe('AppController', () => {
   let appController: AppController;
