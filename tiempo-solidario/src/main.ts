48ddc7fb2a31b2bf60413be377a5d944b2d72111
dianaCiganda <dianaciganda@gmail.com>
2026-06-17 21:25:00 -0300

    test cors all origins

--- tiempo-solidario/src/main.ts
+++ tiempo-solidario/src/main.ts
@@ -24,10 +24,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://tiemposolidario.onrender.com',
-    ],
+    origin:true,
     credentials: true,
   });
 console.log('CORS CONFIGURADO PARA RENDER');
